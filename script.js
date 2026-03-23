e93beae0101625e1ee4bba23c91ca029cb30a2a4
Daayim Mustapha <c24052620@cardiff.ac.uk>
2026-03-23 22:11:23 +0000

    calculator done

--- script.js
+++ script.js
@@ -3,7 +3,21 @@
 
 function buttonClicked(value){
     
-    let display = document.getElementById('runningExpression')
-    let expression = display.innerText + value
+    let display = document.getElementById('total')
+    let expression = display.innerText
+    
+    if (value == 'C'){
+        expression = ''
+    }
+    else if(value == "⌫"){
+    expression = expression.slice(0,-1)
+    }
+    else if (value == '='){
+        expression = eval(expression)
+    } else{
+        expression =  expression + value
+    }   
     return display.innerText = expression
 }
+
+    
